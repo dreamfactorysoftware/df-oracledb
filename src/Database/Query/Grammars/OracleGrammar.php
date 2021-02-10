7609cd47bae0f19bcc6b362bc79e453b530c0c74
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2021-02-10 13:52:11 +0200

    Add pagination performance improvement with laravel 6

--- src/Database/Query/Grammars/OracleGrammar.php
+++ src/Database/Query/Grammars/OracleGrammar.php
@@ -3,11 +3,32 @@
 namespace DreamFactory\Core\Oracle\Database\Query\Grammars;
 
 use Exception;
+use Illuminate\Database\Query\Builder;
 use PDO;
 use Yajra\Pdo\Oci8;
 
 class OracleGrammar extends \Yajra\Oci8\Query\Grammars\OracleGrammar
 {
+    /**
+     * @inheritDoc
+     */
+    protected function compileTableExpression($sql, $constraint, $query)
+    {
+        if ($query->limit == 1 && is_null($query->offset)) {
+            return "select * from ({$sql}) where rownum {$constraint}";
+        }
+
+        if (! is_null($query->limit && ! is_null($query->offset))) {
+            $start  = $query->offset + 1;
+            $finish = $query->offset + $query->limit;
+
+            return "select t2.* from ( select rownum AS \"rn\", t1.* from ({$sql}) t1 where rownum <= {$finish}) t2 where t2.\"rn\" >= {$start}";
+        }
+
+        return "select t2.* from ( select rownum AS \"rn\", t1.* from ({$sql}) t1 ) t2 where t2.\"rn\" {$constraint}";
+    }
+
+
     /**
      * Wrap a single string in keyword identifiers.
      *
